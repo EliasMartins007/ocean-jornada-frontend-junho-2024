0146a0e318bb13fd0c539c0e3ab4250dd82bb961
Elias Martins Dos Reis <eliasfaculdade@yahoo.com.br>
2024-06-06 14:29:41 +0000

    estrutura html

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <>
-      App
+      <div>
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+      </div>
+
+      <div>
+        <h2>Rick Sanchez2</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+      </div>
     </>
   )
 }
