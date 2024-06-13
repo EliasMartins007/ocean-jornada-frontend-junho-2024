99cc09d8caf4a1bea7a2530c4000fdc4f2e456cb
Elias Martins Dos Reis <eliasfaculdade@yahoo.com.br>
2024-06-13 12:56:41 +0000

    requisição com fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,37 @@ import Card from './components/Card/Card'
 function App() {
 
 
-  const item1 = {
-    name: 'Rick',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
+//   const item1 = {
+//     name: 'Rick',
+//     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+//   }
 
-  const item2 = {
-    name: 'Morty',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
+//   const item2 = {
+//     name: 'Morty',
+//     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+//   }
 
   
-  const item3 = {
-    name: 'Summer',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
+//   const item3 = {
+//     name: 'Summer',
+//     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+//   }
 
-const itens = [item1, item2, item3]
+// const itens = [item1, item2, item3]
+
+const itens = []
+
+async function carregarDados(){
+const apiURL = 'https://rickandmortyapi.com/api/character' 
+const response = await fetch(apiURL)
+const body = await response.json()
+
+
+
+console.log(body)
+}
+
+carregarDados()
 
   return (
     <>
