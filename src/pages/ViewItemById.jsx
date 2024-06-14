3a4dee5d0955be1fa0f60b0a2f6cd9ac013eace0
Elias Martins Dos Reis <eliasfaculdade@yahoo.com.br>
2024-06-14 15:04:49 +0000

    parte inicial finalizada

--- src/pages/ViewItemById.jsx
+++ src/pages/ViewItemById.jsx
@@ -1,7 +1,9 @@
 
 import { useEffect, useState } from 'react'
 import Card from '../components/Card/Card'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import './ViewItemById.css'
+
 
 function ViewItemById() {
 const [item, setItem] = useState()
@@ -26,8 +28,48 @@ if(!item){
 
   return (
     <>
+    <div className='view_item'>
+    <Link to="/">Voltar para Home</Link>
      <h1>{item.name}</h1>
-     
+
+      <div className='content'>
+      <div>
+        <img src={item.image}/>
+        </div>
+      <div className='attributes'>
+
+
+      <div className='attribute'>
+       <h2>STATUS</h2> 
+       <p>{item.status}</p>
+       </div>
+        <div className='attribute'>
+       <h2>SPECIES</h2> 
+       <p>{item.species}</p>
+        </div>
+        <div className='attribute'>
+       <h2>STATUS</h2> 
+       <p>{item.status}</p>
+        </div>
+        <div className='attribute'>
+       <h2>GENDER</h2> 
+       <p>{item.gender}</p>
+        </div>
+        <div className='attribute'>
+       <h2>STATUS</h2> 
+       <p>{item.status}</p>
+        </div>
+        <div className='attribute'>
+       <h2>ORIGIN</h2> 
+       <p>{item.origin.name}</p>
+        </div>
+        <div className='attribute'>
+       <h2>LOCATION</h2> 
+       <p>{item.location.name}</p>
+        </div>
+        </div>
+      </div>
+     </div>
     </>
   
   )
